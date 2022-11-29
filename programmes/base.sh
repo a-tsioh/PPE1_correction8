ddc5c60fa82793110b476f1ec56c7c34ba633008
Pierre M <pierre.magistry@univ-amu.fr>
2022-11-29 18:05:20 +0100

    fin exercice 3

--- programmes/base.sh
+++ programmes/base.sh
@@ -18,7 +18,7 @@ then
 	exit
 fi
 
-mot="robot" # à modifier
+mot="[Rr]obots?" # à modifier
 
 echo $fichier_urls;
 basename=$(basename -s .txt $fichier_urls)
@@ -27,7 +27,7 @@ echo "<html><body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
 echo "<br/>" >> $fichier_tableau
 echo "<table>" >> $fichier_tableau
-echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>dump html</th><th>dump text</th></tr>" >> $fichier_tableau
+echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>dump html</th><th>dump text</th><th>occurrences</th></tr>" >> $fichier_tableau
 
 lineno=1;
 while read -r URL; do
@@ -64,7 +64,9 @@ while read -r URL; do
 	fi
   echo "$dump" > "./dumps-text/fich-$lineno.txt"
 
-	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td><a href="../aspirations/fich-$lineno.html">html</a></td><td><a href="../dumps-text/fich-$lineno.txt">text</a></td></tr>" >> $fichier_tableau
+  # compte du nombre d'occurrences
+  NB_OCC=$(grep -E -o $mot ./dumps-text/fich-$lineno.txt | wc -l)
+	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td><a href="../aspirations/fich-$lineno.html">html</a></td><td><a href="../dumps-text/fich-$lineno.txt">text</a></td><td>$NB_OCC</td></tr>" >> $fichier_tableau
 	echo -e "\t--------------------------------"
 	lineno=$((lineno+1));
 done < $fichier_urls
